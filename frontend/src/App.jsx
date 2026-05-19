99dc022b8ddc0f2155bfe03c10ef5873dbd2e460
CZRichardLi <lichengzhe041220@gmail.com>
2026-05-18 22:44:32 -0400

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+function App(){
+  // fake posts for demonstration
+  const posts = [
+    { id: 1, title: "Looking for violinist", author: "Anna" },
+    { id: 2, title: "Film project needs editor", author: "Mike" }
+  ];
+  return (
+    <div>
+      <h1>Arts Forum</h1>
+
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>By {post.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
